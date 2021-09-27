a8331a2bfe643b3de9ac0f6ca2b4b8167aaeb2b8
Jenifer-c <jhennyk.com@gmail.com>
2021-09-27 16:26:40 -0300

    Criado Estilos Globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import styled from 'styled-components';
-
-const Title = styled.h1`
-  font-size: 64px;
-  color: #803ed6;
-`
+import { GlobalStyle } from './styles/global';
 
 function App() {
   return (
-    <div className="App">
-      <Title>Hello World</Title>
-    </div>
+    <div>
+      <h1>Hello World</h1>
+      <GlobalStyle />
+    </div>  
   );
 }
 
