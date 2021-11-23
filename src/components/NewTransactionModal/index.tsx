444528017f29dc34ae56591a5cf3cf44e7589213
Jenifer-c <jhennyk.com@gmail.com>
2021-11-23 14:56:43 -0300

    Feito funcionamento dos Botões

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,9 @@
 import Modal from "react-modal";
-import { Container, TransactionTypeContainer } from "./styles";
+import { Container, RadioBox, TransactionTypeContainer } from "./styles";
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
+import { useState } from "react";
 
 type NewTransactionModalPropsType = {
     isOpen: boolean,
@@ -10,6 +11,8 @@ type NewTransactionModalPropsType = {
 }
 
 export function NewTransactionModal({isOpen, onRequestClose} : NewTransactionModalPropsType) {
+    const [ type, setType ] = useState('deposit')
+    
     return(
         <Modal
             isOpen           = { isOpen }
@@ -37,19 +40,23 @@ export function NewTransactionModal({isOpen, onRequestClose} : NewTransactionMod
                 />
 
                     <TransactionTypeContainer>
-                        <button
-                            type = "button"
+                        <RadioBox
+                            type     = "button"
+                            onClick  = { () => setType('deposit') }
+                            isActive = { type === 'deposit' }
                         >
                             <img src = { incomeImg } alt = "Entrada" />
                             <span>Entrada</span>
-                        </button>
+                        </RadioBox>
 
-                        <button
-                            type = "button"
+                        <RadioBox
+                            type     = "button"
+                            onClick  = {() => setType('withdraw')}
+                            isActive = { type === 'withdraw' }
                         >
                             <img src = { outcomeImg } alt = "Saída" />
                             <span>Saída</span>
-                        </button>
+                        </RadioBox>
                     </TransactionTypeContainer>
 
                 <input 
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -55,12 +55,18 @@ export const TransactionTypeContainer = styled.div`
     grid-template-columns: 1fr 1fr;
     gap: 0.5rem;
 
-    button {
+`;
+
+type RadioBoxProps = {
+    isActive: boolean;
+}
+
+export const RadioBox = styled.button<RadioBoxProps>`
         height: 4rem;
         border: 1px solid #d7d7d7;
         border-radius: 0.25rem;
 
-        background: transparent;
+        background: ${ (props) => props.isActive ? '#ccc' : 'transparent'};
 
         display: flex;
         align-items: center;
@@ -83,5 +89,4 @@ export const TransactionTypeContainer = styled.div`
             font-size: 1rem;
             color: var(--text-title);
         }
-    }
 `;
