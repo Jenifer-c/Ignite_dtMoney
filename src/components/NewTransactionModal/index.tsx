ad4a626e5370476fe86b462607473d5f6b6b07e7
Jenifer-c <jhennyk.com@gmail.com>
2021-11-23 15:16:18 -0300

    Colocado as Cores dos Botões

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -41,18 +41,20 @@ export function NewTransactionModal({isOpen, onRequestClose} : NewTransactionMod
 
                     <TransactionTypeContainer>
                         <RadioBox
-                            type     = "button"
-                            onClick  = { () => setType('deposit') }
-                            isActive = { type === 'deposit' }
+                            type        = "button"
+                            onClick     = { () => setType('deposit') }
+                            isActive    = { type === 'deposit' }
+                            activeColor = "green"
                         >
                             <img src = { incomeImg } alt = "Entrada" />
                             <span>Entrada</span>
                         </RadioBox>
 
                         <RadioBox
-                            type     = "button"
-                            onClick  = {() => setType('withdraw')}
-                            isActive = { type === 'withdraw' }
+                            type        = "button"
+                            onClick     = {() => setType('withdraw')}
+                            isActive    = { type === 'withdraw' }
+                            activeColor = "red"
                         >
                             <img src = { outcomeImg } alt = "Saída" />
                             <span>Saída</span>
